7426d8c804f6ba9a60c9a8b72a20ac793246905f
ShariarShantoDev <iamshanto.dev@gmail.com>
2026-05-07 16:31:46 +0600

    practice variable

---
+++ new_project-1/lib/variable.dart
@@ -0,0 +1,29 @@
+void main() {
+  // print("Hello Shanto");
+
+  var name = "shanto";
+  var id = 221002567;
+  print("Name: $name, Student ID: $id");
+
+  id = 16979;
+  print("Employee ID: $id");
+  // id = "shanto"; // can't change datatype
+
+  // type can change by "dynamic"
+  dynamic p = 23423;
+  print(p);
+  p = true;
+  print(p);
+
+  name = "Al Shahriar Ahommed Shanto";
+  double salary = 20500.00;
+  double cgpa = 3.46;
+  String university = "Green University of Bangladesh";
+  bool isStudent = false;
+
+  print(
+    "Name: $name,\nUniversity name: $university,\nFinal CGPA= $cgpa, Current Studenship: $isStudent,\nSalary: $salary",
+  );
+
+  // const and final 
+}
