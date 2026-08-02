63f54847132cd8d22c8d4b27c7e2ef13f8f1e7d6
ShariarShantoDev <iamshanto.dev@gmail.com>
2026-08-02 22:34:44 +0600

    update main

--- OOP_Practice/class_object/main.dart
+++ OOP_Practice/class_object/main.dart
@@ -21,10 +21,20 @@
 // }
 
 void main() {
+  Student s1 = Student(); 
+  s1.id= 567; 
+  s1.name = "Shanto"; 
+  s1.dept = "CSE"; 
+  s1.cgpa = 2.33; 
+  s1.showDetails(); 
+  print(s1.isPassed() ? "Passed" : "Fail");
 
+  Student s2 = Student(); 
+  s2.cgpa= 3.44; 
+  print(s2.isPassed() ? "Passed" : "Fail");
 }
 
-class student {
+class Student {
   int? id;
   String? name;
   String? dept;
@@ -32,6 +42,13 @@ class student {
 
 
   void showDetails(){
-    print("Student Id: $id")
+    print("Student Id: $id");
+    print("Student Name: $name");
+    print("Student department: $dept");
+    print("Student cgpa: $cgpa");
+  }
+
+  bool isPassed(){
+    return cgpa !=null && cgpa! >= 2.50 ;
   }
 }
